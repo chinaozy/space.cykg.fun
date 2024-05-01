01dd3303ce35ca9672e15e2b0b48085c15addcc0
chinaozy <chinaozy@451419.xyz>
2024-05-02 00:56:21 +0800

    minor update (to make it more lagging)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 //copied from https://stackoverflow.com/questions/52103492/
 var medias = Array.prototype.slice.apply(document.querySelectorAll('audio,video'));
-medias.forEach(function(media) {
-  media.addEventListener('play', function(event) {
-    medias.forEach(function(media) {
-      if(event.target != media) media.pause();
+medias.forEach(function (media) {
+    media.addEventListener('play', function (event) {
+        medias.forEach(function (media) {
+            if (event.target != media) media.pause();
+        });
     });
-  });
 });
+
+let box = document.querySelector('.box')
+box.addEventListener('wheel', (event) => {
+    event.preventDefault()
+    box.scrollLeft += event.deltaY
+})
 
 document.addEventListener("alpine:init", () => {
     Alpine.data("cardData", (card) => ({
@@ -14,27 +20,30 @@ document.addEventListener("alpine:init", () => {
         cardIsOpen: false,
         cardPage: "main",
         cardToggle(item) {
-            if(!this.panelIsUp) this.panelIsUp = true
+            if (!this.panelIsUp) this.panelIsUp = true
             this.nowCard = this.card
             this.cardIsOpen = true
-            this.cardPage = item 
+            this.cardPage = item
         },
         cardOpen() {
-            if(!this.panelIsUp) this.panelIsUp = true
+            if (!this.panelIsUp) this.panelIsUp = true
             this.nowCard = this.card
-            this.cardIsOpen = ! this.cardIsOpen
+            this.cardIsOpen = !this.cardIsOpen
         },
         cardClose() {
-            if(!this.panelIsUp || this.card != this.nowCard) this.cardIsOpen = false
+            if (!this.panelIsUp || this.card != this.nowCard) this.cardIsOpen = false
         },
-        get cardSymbol(){
+        get cardSymbol() {
             return this.cardIsOpen ? "∧" : "∨"
         }
     })),
-    Alpine.data("cardPages", (page) => ({
-        page: page,
-        cardIsShow(){
-            return this.cardIsOpen && this.cardPage == this.page
-        }
+        Alpine.data("cardPages", (page) => ({
+            page: page,
+            cardIsShow() {
+                return this.cardIsOpen && this.cardPage == this.page
+            }
+        }))
+    Alpine.data("mask", () => ({
+        open: true,
     }))
 });
